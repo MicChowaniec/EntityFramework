94c846c2d0273791d502fae51d7dcdbb16bf2beb
michalchowaniec <michal.chowaniec@microsoft.wsei.edu.pl>
2022-09-14 19:27:49 +0200

    Dodaj pliki projektów.

---
+++ Entity/Model.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+public class DBContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+
+    public string ConnectionString { get; }
+    public object Blog { get; internal set; }
+
+    public DBContext(string connectionString)
+    {
+        this.ConnectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(this.ConnectionString);
+    }
+}
+
+public class User
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+
+   // [ForeignKey("RoleId")]
+    public long? RoleId { get; set; }
+
+    //[ForeignKey("TaskId")]
+    public long? TaskId { get; set; }
+}
+
+public class Role
+{
+   // [ForeignKey("RoleId")]
+    public long RoleId { get; set; }
+    public string? Title { get; set; }
+
+
+}
+
+
+public class Task
+{
+   // [ForeignKey("TaskId")]
+    public long TaskId { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+}
---
+++ Entity/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+public class Program
+{
+    public static async System.Threading.Tasks.Task Main()
+    {
+        // Hint: change `DESKTOP-123ABC\SQLEXPRESS` to your server name
+        //       alternatively use `localhost` or `localhost\SQLEXPRESS`
+
+        string connectionString = @"Data Source=DESKTOP-IJAHE4J;Initial Catalog=blogdb;Integrated Security=True";
+
+        using (DBContext db = new DBContext(connectionString))
+        {
+            Console.WriteLine($"Database ConnectionString: {db.ConnectionString}.");
+
+            // Create
+            Console.WriteLine("Making a new johnDoe");
+
+            db.Add(new User { Name="John Doe" });
+            await db.SaveChangesAsync();
+
+            // Read
+            Console.WriteLine("Querying for a blog");
+
+            User user = await db.Users
+                .OrderBy(b => b.Id)
+                .FirstAsync();
+
+            // Update
+            Console.WriteLine("Updating johnDoe ");
+
+            Console.WriteLine("Updating a role, and giving first assignment");
+
+            db.Add(new Role { Title = "Chieftan" });
+            db.Add(new Task { Title = "Job to do", Content = "Clean Your New Office" });
+
+            user.Name = user.Name + "Junior";
+            user.RoleId = user.RoleId + 1;
+            user.TaskId = user.RoleId + 1;
+            Console.WriteLine(user.RoleId +" "+ user.TaskId);
+            
+
+            await db.SaveChangesAsync();
+
+            // Delete
+            Console.WriteLine("Delete johnDoe");
+
+            db.Remove(user);
+            await db.SaveChangesAsync();
+
+            Console.ReadLine();
+        }
+    }
+}
